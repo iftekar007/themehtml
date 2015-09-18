c945fd34ebb856eaad28995fd1185c754c1d3114
iftekar007 <hiifte@gmail.com>
2015-09-18 18:51:06 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Gonnahappen Admin</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+    <link href="css/admin_style.css" type="text/css" rel="stylesheet">
+    <link href="css/media.css" type="text/css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" type="text/css" rel="stylesheet">
+    <script src="js/jQuery-2.1.4.min.js"  type="text/javascript">
+    <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+</head>
+<body>
+
+
+    <div class="container-fluid top_header">
+       <div class="row">
+        <div class="col-md-3 col-lg-3 col-xs-12 col-sm-3 headerleft">
+        <img class="logo" src="images/logo.png"  >
+        </div>
+           <div class="col-md-6 col-md-offset-3 col-lg-6 col-xs-12 col-sm-8" >
+               <div class="row headerright">
+           <div class="inlinediv inputdiv">
+
+
+               <form class="form-search">
+                   <div class="input-append">
+                       <input type="text" class="span2 search-query">
+                       <button type="submit" class="btn"><i class="icon-search"></i></button>
+                   </div>
+               </form>
+
+
+
+           </div>
+
+
+               <div class="inlinediv">
+               <img class="imginline" src="images/user.png">
+               <span>Lorem Ipsum</span>
+                  <br/>
+               <span>Administrator</span>
+               </div>
+                   <div class="clear"></div>
+
+               </div>
+           </div>
+       </div>
+    </div>
+<div class=" container-fluid bottom_header">
+
+<div class="row">
+    <div class="col-md-3 col-lg-3 col-xs-12 col-sm-3 headerleft">
+       <img src="images/key.png">
+        <span>Dashboard</span>
+    </div>
+    <div class=" col-md-6 col-md-offset-3 col-lg-6 col-xs-12 col-sm-8">
+        <nav role="navigation" class="navbar navbar-default">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="#" class="navbar-brand">Brand</a>
+            </div>
+            <!-- Collection of nav links and other content for toggling -->
+            <div id="navbarCollapse" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Messages</a></li>
+                </ul>
+
+            </div>
+        </nav>
+
+    </div>
+</div>
+</div>
+</body>
+</html>
